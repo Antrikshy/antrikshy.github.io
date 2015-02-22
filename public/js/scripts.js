91215b1d21870390fe0ad27046d56369d37ef3ab
Antriksh Yadav <antrikshy@gmail.com>
2015-02-21 16:52:58 -0800

    Set up basic typed.js, added Foundation files

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".intro-text-1").typed({
+        strings: ["Software developer with a voracious curiosity and a desire to learn everything."],
+        typeSpeed: 0,
+    });
+    $(".intro-text-2").typed({
+        strings: ["Computer science student at the University of California, San Diego."],
+        typeSpeed: 0,
+    });
+    $(".intro-text-3").typed({
+        strings: ["\"I believe in not just writing code, but designing great experiences.\""],
+        typeSpeed: 0,
+    });
+});

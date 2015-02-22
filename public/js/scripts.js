925de8c346a99480cb1437ca8ba84aa9c0c58d82
Antriksh Yadav <antrikshy@gmail.com>
2015-02-21 22:19:37 -0800

    Added scrollto, cleaned up some styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
+    $(".scroll-prompter a").hide();
+
     $(".intro-text-1").typed({
         strings: ["Software developer with a voracious curiosity and a desire to learn everything.^1800"],
         typeSpeed: 0,
         callback: function() {
-            $(".typed-cursor").css('display', 'none');
+            $(".typed-cursor").fadeOut("fast");
             $(".intro-text-2").typed({
                 strings: ["Computer science student at the University of California, San Diego.^1800<br>"],
                 typeSpeed: 0,
                 callback: function() {
-                    $(".typed-cursor").css('display', 'none');
+                    $(".typed-cursor").fadeOut("fast");
                     $(".intro-text-3").typed({
                         strings: ["I believe in not just writing code, but designing great experiences."],
                         typeSpeed: 0,
                         callback: function() {
-                            console.log("Typing done.");
+                            $(".scroll-prompter a").fadeIn("slow");
                         }
                     });
                 }
             });
         }
+    });
+
+    $(".scroll-prompter a").click(function() {
+        $(".featured-projects").ScrollTo();
     });
 });

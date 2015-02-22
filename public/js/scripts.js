a6936d4302f3a8bf129be168fa8ef6a369df471c
Antriksh Yadav <antrikshy@gmail.com>
2015-02-21 18:16:58 -0800

    Added Font Awesome etc

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
     $(".intro-text-1").typed({
-        strings: ["Software developer with a voracious curiosity and a desire to learn everything."],
+        strings: ["Software developer with a voracious curiosity and a desire to learn everything.^1800"],
         typeSpeed: 0,
-    });
-    $(".intro-text-2").typed({
-        strings: ["Computer science student at the University of California, San Diego."],
-        typeSpeed: 0,
-    });
-    $(".intro-text-3").typed({
-        strings: ["\"I believe in not just writing code, but designing great experiences.\""],
-        typeSpeed: 0,
+        callback: function() {
+            $(".typed-cursor").css('display', 'none');
+            $(".intro-text-2").typed({
+                strings: ["Computer science student at the University of California, San Diego.^1800<br>"],
+                typeSpeed: 0,
+                callback: function() {
+                    $(".typed-cursor").css('display', 'none');
+                    $(".intro-text-3").typed({
+                        strings: ["I believe in not just writing code, but designing great experiences."],
+                        typeSpeed: 0,
+                        callback: function() {
+                            console.log("Typing done.");
+                        }
+                    });
+                }
+            });
+        }
     });
 });

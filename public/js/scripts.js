cb43d6df6cfb98d48f99b61a155cc462687fc82d
Antriksh Yadav <antrikshy@gmail.com>
2016-03-20 15:09:03 -0700

    Added click-to-skip for intro text

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,7 @@
+var greetingLine1 = "Software developer with a voracious curiosity and a desire to learn everything.";
+var greetingLine2 = "Computer science student at the University of California, San Diego.";
+var greetingLine3 = "I believe in not just <em>writing code</em>, but designing <em>great experiences</em>.";
+
 $(document).ready(function() {
     $(document).foundation();
     new FastClick(document.body);
@@ -15,19 +19,19 @@ $(document).ready(function() {
 
     $(".intro-greeting").hide().slideDown('slow', function() {
         $(".intro-text-1").typed({
-            strings: ["^1500Software developer with a voracious curiosity and a desire to learn everything.^1800"],
+            strings: ["^1500" + greetingLine1 + "^1800"],
             typeSpeed: 0,
             cursorChar: " |",
             callback: function() {
                 $(".typed-cursor").fadeOut("fast");
                 $(".intro-text-2").typed({
-                    strings: ["Computer science student at the University of California, San Diego.^1800<br>"],
+                    strings: [greetingLine2 + "^1800<br>"],
                     typeSpeed: 0,
                     cursorChar: " |",
                     callback: function() {
                         $(".typed-cursor").fadeOut("fast");
                         $(".intro-text-3").typed({
-                            strings: ["I believe in not just <em>writing code</em>, but designing <em>great experiences</em>."],
+                            strings: [greetingLine3],
                             typeSpeed: 0,
                             cursorChar: " |",
                             callback: function() {
@@ -38,5 +42,19 @@ $(document).ready(function() {
                 });
             }
         });
+    });
+
+    $(".intro-text").click(function(e) {
+        e.preventDefault();
+
+        $(".intro-text-1").parent().hide();
+        $(".intro-text-2").parent().hide();
+        $(".intro-text-3").parent().hide();
+
+        $(".intro-text-1-skipped").html(greetingLine1);
+        $(".intro-text-2-skipped").html(greetingLine2);
+        $(".intro-text-3-skipped").html(greetingLine3);
+
+        $(".scroll-prompter").fadeIn("slow");
     });
 });
